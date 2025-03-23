4c442550489d32184d4388cec524fb27285f83ee
Freezebee <stanislavlav05@gmail.com>
2025-03-23 15:50:28 +0300

    feat: updating page state

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 import { Select } from '../select';
-import { backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
 import { RadioGroup } from '../radio-group';
 import { Spacing } from '../spacing';
 import { Separator } from '../separator';
@@ -18,17 +18,19 @@ const optionsBGColor = backgroundColors;
 const optionsWidth = contentWidthArr;
 const spacing = 30;
 
-export const ArticleParamsForm = () => {
+
+export const ArticleParamsForm = ({setState} : {setState: React.Dispatch<React.SetStateAction<ArticleStateType>>}) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [selectedFont, setSelectedFont] = useState(optionsFont[0]);
-	const [selectedSize, setSelectedSize] = useState(optionsSize[0]);
-	const [selectedColor, setSelectedColor] = useState(optionsColor[0]);
-	const [selectedBGColor, setSelectedBGColor] = useState(optionsBGColor[0]);
-	const [selectedWidth, setSelectedWidth] = useState(optionsWidth[0]);
+	const [formState, setFormState] = useState<ArticleStateType>(defaultArticleState);
 
 	const handleBtnClick = () => {
 		setIsOpen(prev => !prev);
-	}
+	};
+
+	const handleFormSubmit = (ev: React.SyntheticEvent) => {
+		ev.preventDefault();
+		setState(formState);
+	};
 
 
 	return (
@@ -38,26 +40,41 @@ export const ArticleParamsForm = () => {
 				className={clsx(styles.container, {
 					[styles.container_open]: isOpen,
 				})}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
 					<Select	
 						options={optionsFont}
-						onChange={setSelectedFont}
-						selected={selectedFont}
+						onChange={(selected) =>
+							setFormState((prev) => ({
+								...prev,
+								fontFamilyOption: selected,
+							}))
+						}
+						selected={formState.fontFamilyOption}
 						title="Шрифт"
 					/>
 					<Spacing size={spacing} />
 					<RadioGroup
-						selected={selectedSize}
+						selected={formState.fontSizeOption}
 						name='radio'
-						onChange={setSelectedSize}
+						onChange={(selected) =>
+							setFormState((prev) => ({
+								...prev,
+								fontSizeOption: selected,
+							}))
+						}
 						options={optionsSize}
 						title='размер шрифта'
 					/>
 					<Spacing size={spacing} />
 					<Select	
 						options={optionsColor}
-						onChange={setSelectedColor}
-						selected={selectedColor}
+						onChange={(selected) =>
+							setFormState((prev) => ({
+								...prev,
+								fontColor: selected,
+							}))
+						}
+						selected={formState.fontColor}
 						title="цвет шрифта"
 					/>
 					<Spacing size={spacing} />
@@ -65,15 +82,25 @@ export const ArticleParamsForm = () => {
 					<Spacing size={spacing} />
 					<Select	
 						options={optionsBGColor}
-						onChange={setSelectedBGColor}
-						selected={selectedBGColor}
+						onChange={(selected) =>
+							setFormState((prev) => ({
+								...prev,
+								backgroundColor: selected,
+							}))
+						}
+						selected={formState.backgroundColor}
 						title="цвет фона"
 					/>
 					<Spacing size={spacing} />
 					<Select	
 						options={optionsWidth}
-						onChange={setSelectedWidth}
-						selected={selectedWidth}
+						onChange={(selected) =>
+							setFormState((prev) => ({
+								...prev,
+								contentWidth: selected,
+							}))
+						}
+						selected={formState.contentWidth}
 						title="ширина контента"
 					/>
 
@@ -82,7 +109,7 @@ export const ArticleParamsForm = () => {
 							title='Сбросить'
 							type='reset'
 						/>
-						<Button title='Применить' type='submit' />
+						<Button title='Применить' type='submit'/>
 					</div>
 				</form>
 			</aside>
